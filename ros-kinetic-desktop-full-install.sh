d9b8ce5e2468762fc54b92bb29e793a93e1fda3d
winstxnhdw <56998716+winstxnhdw@users.noreply.github.com>
2020-05-26 21:47:59 +0800

    Add files via upload

---
+++ ros-kinetic-desktop-full-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+# Setup sources list and keys
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+# Install ROS Kinetic
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full
+
+# Setup Environment
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+source /opt/ros/kinetic/setup.bash
+
+# Install the ROS dependencies for building packages
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install python-rosdep
+sudo rosdep init
+rosdep update
