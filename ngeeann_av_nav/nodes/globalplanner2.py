1e743be05e1b5d525c95b3174ab242ede87723a7
Reuben Thomas <reuben-thomas>
2020-08-06 08:50:20 +0800

    working gp

--- ngeeann_av_nav/nodes/globalplanner2.py
+++ ngeeann_av_nav/nodes/globalplanner2.py
@@ -4,6 +4,7 @@ import rospy, os
 import numpy as np
 import pandas as pd
 import PyKDL
+import threading
 
 from geometry_msgs.msg import Pose2D, Quaternion, Pose, PoseArray
 from nav_msgs.msg import Path
@@ -29,15 +30,8 @@ class GlobalPathPlanner:
         try:
             self.global_planner_params = rospy.get_param("/global_path_planner")
             self.frequency = self.global_planner_params["update_frequency"]
-            self.givenwp = self.global_planner_params["given_number_of_waypoints"]
-            self.tolerance = self.global_planner_params["target_tolerance"]
             self.tracker_params = rospy.get_param("/path_tracker")
             self.cg2frontaxle = self.tracker_params["centreofgravity_to_frontaxle"]
-
-            if self.givenwp < 2:
-                self.givenwp == 2
-            else:
-                pass
         except:
             raise Exception("Missing ROS parameters. Check the configuration file.")
 
@@ -52,8 +46,7 @@ class GlobalPathPlanner:
         self.ax = df['X-axis'].values.tolist()
         self.ay = df['Y-axis'].values.tolist()
 
-        # Class variables to use whenever within the class when necessary
-        self.alive = False
+        self.lock = threading.Lock()
 
         self.x = None
         self.y = None
@@ -63,20 +56,23 @@ class GlobalPathPlanner:
         ''' Callback function to check if the Local Path Planner has been initialised '''
 
         if msg.data == "I am alive!":
-            self.alive = True
-        
+            self.alive = True 
         else:
             self.alive = False
 
     def vehicle_state_cb(self, msg):
         ''' Callback function to update vehicle state '''
-
         self.x = msg.pose.x
         self.y = msg.pose.y
         self.theta = msg.pose.theta
-        self.find_closest_point()
 
-    def find_closest_point(self):
+    def set_waypoints(self):
+        ''' Determines the appropriate set of waypoints to publish by the following steps
+
+            1. Identify waypoint closest to front axle
+            2. Determines if this point is ahead or behind, by transformation
+            3. Preserves at least 1 point behind, 5 points ahead at all times
+        '''
         # Identify position of vehicle front axle
         fx = self.x + self.cg2frontaxle * np.cos(self.theta + np.pi*0.5)
         fy = self.y + self.cg2frontaxle * np.sin(self.theta + np.pi*0.5)
@@ -84,35 +80,43 @@ class GlobalPathPlanner:
         dx = [fx - icx for icx in self.ax] # Find the x-axis of the front axle relative to the path
         dy = [fy - icy for icy in self.ay] # Find the y-axis of the front axle relative to the path
 
-        d = np.hypot(dx, dy) # Find the distance from the front axle to the path
-        closest_id = np.argmin(d) # Find the shortest distance in the array
+        d = np.hypot(dx, dy)        # Find the distance from the front axle to the path
+        closest_id = np.argmin(d)   # Find the shortest distance in the array
 
-        self.frame_transform(closest_id, fx, fy)
+        transform = self.frame_transform(self.ax[closest_id], self.ay[closest_id], fx, fy, self.theta)
+        
+        if (transform[1] <= 0.0):
+        # If the vehicle has passed, closest point is preserved as a point behind the car
+            px = self.ax[closest_id - 1 : (closest_id + 4)]
+            py = self.ay[closest_id - 1: (closest_id + 4)]
+            print('Closest Waypoint #: {} (Passed)'.format(closest_id))
 
-    def frame_transform(self, closest_id, axle_x, axle_y):
+        else:
+        # If the vehicle has yet to pass, a point behind the closest is preserved as a point behind the car
+            px = self.ax[(closest_id - 2) : (closest_id + 3)]
+            py = self.ay[(closest_id - 2) : (closest_id + 3)]
+            print('Closest Waypoint #: {} (Approaching)'.format(closest_id))
+        
+        self.publish_goals(px, py)
+
+    def frame_transform(self, point_x, point_y, axle_x, axle_y, theta):
         ''' Recieves position of vehicle front axle, and id of closest waypoint. This waypoint is transformed from
             "map" frame to the vehicle frame
 
             Arguments:
-                closest_id      - Index to closest waypoint to front axle in master waypoint list
-                axle_x, axle_y  - Coordinates (x,y) of vehicle front axle position
+                closest_id          - Index to closest waypoint to front axle in master waypoint list
+                point_x, point_y    - Coordinates (x,y) of vehicle front axle position
+                axle_x, axle_y      - Coordinates (x,y) of vehicle front axle position
         '''
+        c, s = np.cos(-theta), np.sin(-theta)     # Creates rotation matrix given theta
+        R = np.array(((c, -s), (s, c)))  
 
-        # Creates vehicle frame
-        vehicle_frame = PyKDL.Frame(PyKDL.Rotation.RPY(0, 0, self.theta + (np.pi*0.5)), PyKDL.Vector(axle_x, axle_y, 0.0))
-        print('Current Yaw Angle: {}'.format(self.theta))
+        p = np.array(((point_x), (point_y)))      # Position vector of closest waypoint (world frame)
+        v = np.array(((axle_x), (axle_y)))        # Position vector of vehicle (world frame)
+        vp = p - v                                # Linear translation between vehicle and point     
 
-        # Position vector of closest waypoint in map frame
-        p_map = PyKDL.Vector(self.ax[closest_id], self.ay[closest_id], 0.0)
-
-        # Transformation of waypoint to vehicle frame
-        p_vehicle = vehicle_frame * p_map
-        print('Waypoint #: {}'.format(closest_id))
-        #print('Vehicle frame: {}'.format(vehicle_frame))
-        print('Map ref      : {}'.format(p_map))
-        print('Vehicle ref  : {}'.format(p_vehicle))
-
-
+        transform = R.dot(vp)      # Product of rotation matrix and translation vector
+        return transform
 
     def publish_goals(self, ax, ay):
         ''' Publishes an array of waypoints for the Local Path Planner '''
@@ -135,6 +139,7 @@ class GlobalPathPlanner:
             vpose.position.x = ax[i]
             vpose.position.y = ay[i]
             vpose.position.z = 0.0
+            vpose.orientation
             viz_goals.poses.append(vpose)
         
         self.goals_pub.publish(goals)
@@ -142,7 +147,7 @@ class GlobalPathPlanner:
 
         print("\n")
         print("Total goals published: {}".format(len(ax)))
-        print("\nPublished goals: \n{}".format(goals))
+        #print("\nPublished goals: \n{}".format(goals))
 
     def walk_up_folder(self, path, dir_goal='fyp-moovita'):
         ''' Searches and returns the directory of the waypoint.csv file ''' 
@@ -166,12 +171,14 @@ def main():
     # Initialise the node
     rospy.init_node('global_planner')
 
+    rospy.wait_for_message('/ngeeann_av/state2D', State2D)
+
     # Set update rate
     r = rospy.Rate(global_planner.frequency)
 
     while not rospy.is_shutdown():
         try:
-            global_planner.publish_goals(global_planner.ax, global_planner.ay)
+            global_planner.set_waypoints()
             r.sleep()
 
         except KeyboardInterrupt:
