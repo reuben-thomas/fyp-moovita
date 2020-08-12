edf748b7c765c216d2008e941c9b7018b9c2f517
winstnxhdw <winstxnhdw@gmail.com>
2020-08-12 17:29:14 +0800

    update .git ignore

--- LaneNetRos/scripts/lanenet_node.py
+++ LaneNetRos/scripts/lanenet_node.py
@@ -1,32 +1,19 @@
 #!/usr/bin/env python2
-# -*- coding: UTF-8 -*-
-# @Author  : Luo Yao
-# @Modified  : AdamShan
-# @Original site    : https://github.com/MaybeShewill-CV/lanenet-lane-detection
-# @File    : lanenet_node.py
 
-
-import time
-import math
+import time, math, rospy
 import tensorflow as tf
 import numpy as np
-import cv2
+import cv2 as cv
 
 from lanenet_model import lanenet
 from lanenet_model import lanenet_postprocess
 from config import global_config
-
-import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import Header
 from cv_bridge import CvBridge, CvBridgeError
 from lane_detector.msg import Lane_Image
 
-
-CFG = global_config.cfg
-
-
-class lanenet_detector:
+class LaneNetDetector:
 
     def __init__(self):
 
@@ -37,7 +24,7 @@ class lanenet_detector:
         self.pub_laneimage = rospy.Publisher(self.lane_image_topic, Lane_Image, queue_size=1)
 
         # Initialise subscriber(s)
-        sub_image = rospy.Subscriber(self.image_topic, Image, self.img_callback, queue_size=1)
+        sub_image = rospy.Subscriber(self.image_topic, Image, self.img_cb, queue_size=1)
 
         try: 
             self.image_topic = rospy.get_param('~image_topic')
@@ -50,6 +37,7 @@ class lanenet_detector:
         except:
             raise Exception("Missing ROS parameters. Check the configuration file.")
 
+        self.CFG = global_config.cfg
         self.init_lanenet()
         self.bridge = CvBridge()
     
@@ -57,7 +45,6 @@ class lanenet_detector:
         '''
         initlize the tensorflow model
         '''
-
         self.input_tensor = tf.placeholder(dtype=tf.float32, shape=[1, 256, 512, 3], name='input_tensor')
         phase_tensor = tf.constant('test', tf.string)
         net = lanenet.LaneNet(phase=phase_tensor, net_flag='vgg')
@@ -67,61 +54,68 @@ class lanenet_detector:
         self.postprocessor = lanenet_postprocess.LaneNetPostProcessor()
 
         saver = tf.train.Saver()
+
         # Set sess configuration
         if self.use_gpu:
             sess_config = tf.ConfigProto(device_count={'GPU': 1})
+
         else:
             sess_config = tf.ConfigProto(device_count={'CPU': 0})
-        sess_config.gpu_options.per_process_gpu_memory_fraction = CFG.TEST.GPU_MEMORY_FRACTION
-        sess_config.gpu_options.allow_growth = CFG.TRAIN.TF_ALLOW_GROWTH
+
+        sess_config.gpu_options.per_process_gpu_memory_fraction = self.CFG.TEST.GPU_MEMORY_FRACTION
+        sess_config.gpu_options.allow_growth = self.CFG.TRAIN.TF_ALLOW_GROWTH
         sess_config.gpu_options.allocator_type = 'BFC'
 
         self.sess = tf.Session(config=sess_config)
         saver.restore(sess=self.sess, save_path=self.weight_path)
 
     
-    def img_callback(self, data):
+    def img_cb(self, data):
+
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+
         except CvBridgeError as e:
             print(e)
-        # cv2.namedWindow("ss")
-        # cv2.imshow("ss", cv_image)
-        # cv2.waitKey(0)
+
+        # cv.namedWindow("ss")
+        # cv.imshow("ss", cv_image)
+        # cv.waitKey(0)
         original_img = cv_image.copy()
         resized_image = self.preprocessing(cv_image)
         mask_image = self.inference_net(resized_image, original_img)
-        out_img_msg = self.bridge.cv2_to_imgmsg(mask_image, "bgr8")
+        out_img_msg = self.bridge.cv_to_imgmsg(mask_image, "bgr8")
         self.pub_image.publish(out_img_msg)
         
     def preprocessing(self, img):
-        image = cv2.resize(img, (512, 256), interpolation=cv2.INTER_LINEAR)
+
+        image = cv.resize(img, (512, 256), interpolation=cv.INTER_LINEAR)
         image = image / 127.5 - 1.0
-        # cv2.namedWindow("ss")
-        # cv2.imshow("ss", image)
-        # cv2.waitKey(1)
+        # cv.namedWindow("ss")
+        # cv.imshow("ss", image)
+        # cv.waitKey(1)
         return image
 
     def inference_net(self, img, original_img):
+
         binary_seg_image, instance_seg_image = self.sess.run([self.binary_seg_ret, self.instance_seg_ret],
                                                         feed_dict={self.input_tensor: [img]})
 
-        postprocess_result = self.postprocessor.postprocess(
+        postprocess_result = self.postprocessor.postprocess \
+        (
             binary_seg_result=binary_seg_image[0],
             instance_seg_result=instance_seg_image[0],
             source_image=original_img
         )
         # mask_image = postprocess_result['mask_image']
         mask_image = postprocess_result
-        mask_image = cv2.resize(mask_image, (original_img.shape[1],
-                                                original_img.shape[0]),interpolation=cv2.INTER_LINEAR)
-        mask_image = cv2.addWeighted(original_img, 0.6, mask_image, 5.0, 0)
+        mask_image = cv.resize(mask_image, (original_img.shape[1], original_img.shape[0]),interpolation=cv.INTER_LINEAR)
+        mask_image = cv.addWeighted(original_img, 0.6, mask_image, 5.0, 0)
         return mask_image
 
 
     def minmax_scale(self, input_arr):
         """
-
         :param input_arr:
         :return:
         """
@@ -132,10 +126,11 @@ class lanenet_detector:
 
         return output_arr
 
+def main():
 
+    rospy.init_node('lanenet_node')
+    detector = LaneNetDetector()
+    rospy.spin()
 
 if __name__ == '__main__':
-    # init args
-    rospy.init_node('lanenet_node')
-    lanenet_detector()
-    rospy.spin()
+    main()
